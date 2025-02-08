53692b8ca54b4142e45ecec50a2c8050e9c84b4d
saleh-expansionjs <saleh@expansionjs.com>
2025-02-09 04:24:37 +0530

    created backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '1234',
+        database: 'event_planner_db',
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true
+      }),
+    }),
+    UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
