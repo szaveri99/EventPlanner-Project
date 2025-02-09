386c4dfd10b8c4843b92c6a1ad95ef54520eb84a
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2025-02-09 11:56:09 +0530

    Feature/add ts props (#4)
    * removed .vscode
    
    * changed to ts
    
    * Add typescript props in components and pages
    
    ---------
    
    Co-authored-by: saleh-expansionjs <saleh@expansionjs.com>
    Co-authored-by: Sakina Zaveri <64473621+szaveri99@users.noreply.github.com>

--- frontend/src/component/UserForm.tsx
+++ frontend/src/component/UserForm.tsx
@@ -1,25 +1,31 @@
 import React from "react";
 
-function UserForm({ register, errors }) {
+import { FormProps, BaseFormData } from "../types/FormTypes.types";
+
+const UserForm: React.FC<FormProps<BaseFormData>> = ({ register, errors }) => {
   return (
     <>
-      <input 
-        type="email" 
-        className="form-control mb-3" 
-        placeholder="Type your email"
-        {...register("email", { required: "Email is required", pattern: { value: /\S+@\S+\.\S+/, message: "Enter a valid email" } })}
-      />
-      {errors.email && <p className="text-danger">{errors.email.message}</p>}
+      <form>
+        <input
+          type="email"
+          className="form-control mb-3"
+          placeholder="Type your email"
+          {...register("email", { required: "Email is required" })}
+        />
+        {errors.email && <p className="text-danger">{errors.email.message}</p>}
 
-      <input 
-        type="password" 
-        className="form-control mb-3" 
-        placeholder="Type your password"
-        {...register("password", { required: "Password is required", minLength: { value: 6, message: "Password must be at least 6 characters" } })}
-      />
-      {errors.password && <p className="text-danger">{errors.password.message}</p>}
+        <input
+          type="password"
+          className="form-control mb-3"
+          placeholder="Type your password"
+          {...register("password", { required: "Password is required" })}
+        />
+        {errors.password && (
+          <p className="text-danger">{errors.password.message}</p>
+        )}
+      </form>
     </>
   );
-}
+};
 
 export default UserForm;
--- frontend/src/pages/Signup/Signup.tsx
+++ frontend/src/pages/Signup/Signup.tsx
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useForm, UseFormRegister, FieldErrors } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.min.css";
 import VendorForm from "../../component/VendorForm";
 import UserForm from "../../component/UserForm";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { BaseFormData, VendorFormData } from "../../types/FormTypes.types"; 
 
 function SignUp() {
-  const [role, setRole] = useState("user");
+  const [role, setRole] = useState<"user" | "vendor">("user");
 
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<BaseFormData | VendorFormData>();
 
-  const onSubmit = (data) => {
+  const onSubmit = (data: BaseFormData | VendorFormData) => {
     console.log("Form Data:", data);
     alert("Form submitted successfully!");
     reset(); // Reset form after submission
   };
 
   return (
-    <div className="container-fluid d-flex" style={{overflow:'none',height:'100vh'}}>
+    <div
+      className="container-fluid d-flex"
+      style={{ overflow: "none", height: "100vh" }}
+    >
       <div className="col-md-6 d-flex justify-content-center align-items-center">
         <div className="w-50 d-flex flex-column align-items-center justify-content-center">
           <h2 className="fw-bold">Create an account</h2>
@@ -28,14 +37,23 @@ function SignUp() {
             <button
               className={`btn ${role === "vendor" ? "btn-dark" : "btn-light"}`}
               onClick={() => setRole("vendor")}
-              style={{ border: '1px solid rgba(229,229,229,1)', borderRadius: '5px' }}
+
+              style={{
+                border: "1px solid rgba(229,229,229,1)",
+                borderRadius: "5px",
+              }}
+
             >
               I'm a vendor
             </button>
             <button
               className={`btn ${role === "user" ? "btn-dark" : "btn-light"}`}
               onClick={() => setRole("user")}
-              style={{ border: '1px solid rgba(229,229,229,1)', borderRadius: '5px' }}
+
+              style={{
+                border: "1px solid rgba(229,229,229,1)",
+                borderRadius: "5px",
+              }}
             >
               I'm a user
             </button>
@@ -44,9 +62,17 @@ function SignUp() {
           {/* Form */}
           <form onSubmit={handleSubmit(onSubmit)} className="w-100">
             {role === "vendor" ? (
-              <VendorForm register={register} errors={errors} />
+
+              <VendorForm
+                register={register as UseFormRegister<VendorFormData>}
+                errors={errors as FieldErrors<VendorFormData>}
+              />
             ) : (
-              <UserForm register={register} errors={errors} />
+              <UserForm
+                register={register as UseFormRegister<BaseFormData>}
+                errors={errors as FieldErrors<BaseFormData>}
+              />
+
             )}
 
             {/* Checkbox */}
@@ -54,28 +80,51 @@ function SignUp() {
               <input
                 type="checkbox"
                 className="me-2"
-                {...register("terms", { required: "You must agree to the terms" })}
+                {...register("terms", {
+                  required: "You must agree to the terms",
+                })}
               />
               <span>I agree to terms and services</span>
             </div>
-            {errors.terms && <p className="text-danger">{errors.terms.message}</p>}
+            {errors.terms && (
+              <p className="text-danger">{(errors.terms)?.message}</p>
+            )}
 
             {/* Submit Button */}
-            <button type="submit" className="btn btn-secondary w-100">Sign Up</button>
+            <button type="submit" className="btn btn-secondary w-100">
+              Sign Up
+            </button>
           </form>
 
           {/* Login Link */}
-          <p className="mt-3">Already have an account? <Link to="/login">Log In</Link></p>
+          <p className="mt-3">
+            Already have an account? <Link to="/login">Log In</Link>
+          </p>
+
         </div>
       </div>
 
       {/* Right Side Branding */}
-      <div className="col-md-6 d-flex align-items-center justify-content-center text-white bg-image" 
-        style={{ backgroundImage: 'url(signUpBg.jpeg)', backgroundPosition: 'center', backgroundSize: 'cover' }}>
+
+      <div
+        className="col-md-6 d-flex align-items-center justify-content-center text-white bg-image"
+        style={{
+          backgroundImage: "url(signUpBg.jpeg)",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
         <div className="text-center w-50">
-          <img src="https://c3093a5abab386b0d9690b47b31abedb.cdn.bubble.io/f1709042887076x661658487365572900/Workplace-light.svg" alt="" />
+          <img
+            src="https://c3093a5abab386b0d9690b47b31abedb.cdn.bubble.io/f1709042887076x661658487365572900/Workplace-light.svg"
+            alt=""
+          />
           <h2 className="fw-bold">EventPlanner Pro</h2>
-          <p>Welcome to EventPlanner Pro, Simplifying event planning, handling every detail with ease.</p>
+          <p>
+            Welcome to EventPlanner Pro, Simplifying event planning, handling
+            every detail with ease.
+          </p>
+
         </div>
       </div>
     </div>
