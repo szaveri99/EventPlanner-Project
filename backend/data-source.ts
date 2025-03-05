c7d198dd3276b10db6dcb7ea3de46a88dbf8fcd4
saleh-expansionjs <saleh@expansionjs.com>
2025-03-05 17:40:11 +0530

    fixed issue

--- backend/data-source.ts
+++ backend/data-source.ts
@@ -1,12 +1,15 @@
 const { DataSource } = require('typeorm');
+import { configDotenv } from "dotenv";
+
+configDotenv();
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "1234",
-    database: "event_planner_db",
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: false,
     migrations: ['src/database/migrations/*.ts'],
     entities: ['src/**/*.entity.ts'],
