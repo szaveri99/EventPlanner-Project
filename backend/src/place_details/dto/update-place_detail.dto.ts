8f3e3b2435e0ab4d77b719efc7a6b170c362c4a6
saleh-expansionjs <saleh@expansionjs.com>
2025-04-24 00:58:07 +0530

    changed directory

--- backend/src/place_details/dto/update-place_detail.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePlaceDetailDto } from './create-place_detail.dto';
-
-export class UpdatePlaceDetailDto extends PartialType(CreatePlaceDetailDto) {}
